859b625c374e47b8e310ddd052a2c25cc739af69
Matthew De Leon <mattjdeleon@gmail.com>
2023-11-07 16:30:45 -0700

    building grid component

---
+++ my-app/src/components/gridComponent.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+class Grid extends React.Component {
+  render() {
+    const numRows = 3
+    const numCols = 3
+
+    const cellStyle = {
+      width: '200px',
+      height: '200px',
+      border: '1px solid black',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontSize: '24px',
+    }
+
+    const grid = []
+
+    for (let i = 0; i < numRows; i++) {
+      const row = []
+      for (let j = 0; j < numCols; j++) {
+        const key = `${i}-${j}`
+        row.push(
+          <div key={key} style={cellStyle}>
+            {i * numCols + j + 1}
+          </div>,
+        )
+      }
+      grid.push(
+        <div key={i} style={{ display: 'flex' }}>
+          {row}
+        </div>,
+      )
+    }
+
+    return <div className="grid">{grid}</div>
+  }
+}
+
+export default Grid
