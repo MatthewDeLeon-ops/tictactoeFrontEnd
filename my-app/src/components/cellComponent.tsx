7b30e5354fc0acd77220ae9f8141efd0b0e2ec72
Matthew De Leon <mattjdeleon@gmail.com>
2023-11-22 14:33:21 -0600

    added in sectional comments

--- my-app/src/components/cellComponent.tsx
+++ my-app/src/components/cellComponent.tsx
@@ -1,23 +1,26 @@
-/*
-Add onclick add text to the Square highlighted
-*/
 //!  React, component names must start with a capital letter
 
 import React, { ReactNode } from 'react'
 import '../components/cellsStyle.css'
+/* //* Using cellStyles.css for styling my components */
+
 interface squareBlocks {
   children: ReactNode
-}
-
-const TilesComponent: React.FC<squareBlocks> = ({ children }) => {
-  return <div className="tiles">{children}</div>
 }
 
 interface RowProps {
   children: React.ReactNode
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+/* //? Renders a <div> with the class 'tiles' and  its children. */
+//! The hover effect defined in my css file is applied to all elements with the class tiles, including those in my TilesComponent*/
+
+const TilesComponent: React.FC<squareBlocks> = ({ children }) => {
+  return <div className="tiles">{children}</div>
+}
+
+//? Renders a <div> with a style object defining display properties. This functionally ensures that the squares are displayed in a row. */
+
 const RowComponent: React.FC<RowProps> = ({ children }) => {
   const rowStyle: React.CSSProperties = {
     clear: 'both',
@@ -26,6 +29,8 @@ const RowComponent: React.FC<RowProps> = ({ children }) => {
 
   return <div style={rowStyle}>{children}</div>
 }
+
+//? Layout renders a <div> with the class 'board' containing each row segment which contains each tile in it.
 
 const Layout = () => {
   return (
@@ -50,3 +55,12 @@ const Layout = () => {
 }
 
 export default Layout
+
+/* //* idea for implementing win loss condition */
+/*
+? Create Logic for the ordered Tiles for win/lose conditions
+? ACROSS Tiles: [1,2,3] [4,5,6] [7,8,9]   [x or o] 
+? diagonal: [7,5,3] [9,5,1]               [x or o]
+? Vertical: [1,4,7] [2,5,8] [3,6,9]       [x or o]
+! Any other combination is considered a loss
+*/
--- my-app/src/components/gridComponent.tsx
+++ my-app/src/components/gridComponent.tsx
@@ -6,6 +6,7 @@ class Grid extends React.Component {
     const numRows = 3
     const numCols = 3
 
+    //* Defining styling properties for the grid dimensions (numRows and numCols) and styling for the cells in the grid */
     const cellStyle = {
       width: '200px',
       height: '200px',
@@ -16,27 +17,31 @@ class Grid extends React.Component {
       fontSize: '24px',
     }
 
-    const grid = []
+    const grid: any[] = []
 
-    for (let i = 0; i < numRows; i++) {
-      const row = []
+    //* Generation of the grid
+    //* use nested loops to generate a 3x3 grid of cells. Each cell is a <div> element with a unique key and styled using the cellStyle. The content of each cell is determined by the formula {index * numCols + j + 1}, which generates sequential numbers for each cell. The generated rows are wrapped in an outer <div> with a display: 'flex' style to ensure they are displayed in a ro */
+    for (let index = 0; index < numRows; index++) {
+      const row: any[] = []
       for (let j = 0; j < numCols; j++) {
-        const key = `${i}-${j}`
+        const key = `${index}-${j}`
         row.push(
           <div key={key} style={cellStyle}>
-            {i * numCols + j + 1}
+            {index * numCols + j + 1}
           </div>,
         )
       }
       grid.push(
-        <div key={i} style={{ display: 'flex' }}>
+        <div key={index} style={{ display: 'flex' }}>
           {row}
         </div>,
       )
     }
 
+    //* Rendering the Grid Component
     return <div className="grid">{grid}</div>
   }
 }
 
+//* making the component accessible to other parts of code
 export default Grid
